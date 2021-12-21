7ccc65e5181bebb7e41fe3ed6cff98f77c78c97e
Sukhchain Singh <gillv625@gmail.com>
2021-12-21 23:57:21 +0100

    change to lowDb

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const express = require('express')
+import express from 'express';
+import { join, dirname } from 'path'
+import { Low, JSONFile } from 'lowdb'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+// Use JSON file for storage
+const file = join(__dirname, 'db.json')
+const adapter = new JSONFile(file)
+const db = new Low(adapter)
+// await db.read()
+
+db.data ||= { user: [] }
 
 const app = express();
 
@@ -8,8 +21,7 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.static(__dirname + '/public'));
 
 var dict = {};
-var dictx = []
-app.post('/upload', (req, res) => {
+app.post('/upload', async (req, res) => {
     let name = req.body.name.trim()
     console.log('Got body:', req.body.name + " at " + req.body.time);
 
@@ -18,27 +30,67 @@ app.post('/upload', (req, res) => {
         img: req.body.img
     };
 
-    dictx.push({
+    console.log(Object.keys(dict));
+
+    //start
+    let jayson = {
+        name: name,
         time: req.body.time,
         img: req.body.img
-    })
-    console.log(Object.keys(dict));
+    };
+    const i = db.data.user.findIndex(u => u.name === name);
+    console.log("i is :" + i);
+    if (i === -1) {
+        await db.data.user.push(jayson)
+    } else {
+        db.data.user[i] = jayson;
+    }
+
+    await db.write();
+    //end
+
     res.sendStatus(200);
 });
 
 app.get('/body', (req, res) => {
-    res.send(JSON.stringify(dict));
+    res.send(JSON.stringify(db.data.user));
+
+    // res.send(JSON.stringify(dict));
 });
-app.get('/dicts', (req, res) => {
-    res.send(JSON.stringify(dictx));
+app.get('/db/read', async (req, res) => {
+    // let gg = await db.data.user
+    // console.log(gg)
+    // res.send((gg));
+
+    const i = db.data.user.findIndex(u => u.name === "THANA SINGH");
+    if (i === -1) res.send(db.data.user[i])
+    else res.send("no hay nada")
+
+    // await db.write();
 });
+app.get('/db/write', async (req, res) => {
+    // await db.data.user.push({ name: "THANA SINGH", time: "sd", img: "<img src='data:12453'></img>" });
+    await db.data.user.push({ name: "THANA GILL", time: "sds", img: "<img src='data:12453'></img>" });
+    await db.write();
+    res.send("ok");
+});
+
 app.get('/cache/:query', async (req, res) => {
     let name = req.params.query.trim()
-    if (dict[name])
-        res.send(dict[name].img)
-    else
-        res.send("NOT FOUND")
+    // if (dict[name])
+    //     res.send(dict[name].img)
+    // else
+    //     res.send("NOT FOUND")
 
+
+    const i = db.data.user.findIndex(u => u.name === name);
+    if (i === -1) {
+        res.send("NOT FOUND.")
+
+    } else {
+        res.send("<div>" + db.data.user[i].img + "<p>Actualizado: " + db.data.user[i].time + "</p></div>")
+        await db.write();
+    }
 })
 const port = process.env.PORT || 3001
 
